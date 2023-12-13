e86457ca936c1af79316b983a79b2f149b98aa72
Bharat <Bharat@ikcontech.com>
2023-12-13 18:13:38 +0530

    first-commit

---
+++ src/main/java/com/ikn/hms/HmsConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.ikn.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class HmsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsConfigServerApplication.class, args);
+	}
+
+}
